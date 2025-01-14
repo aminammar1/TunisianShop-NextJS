42a4722a82e023bb6124ccc75fe5e7fbfc920b40
aminammar1 <ammar.mohamdamine@gmail.com>
2025-01-14 22:34:34 +0100

    add login page component and update header

--- client/components/header/Header.jsx
+++ client/components/header/Header.jsx
@@ -1,18 +1,18 @@
-"use client";
+'use client'
 
-import React, { useEffect, useState } from "react";
-import Link from "next/link";
-//import Search from "../search/Search";
+import React, { useEffect, useState } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import Search from '../search/Search'
 //import UserMenu from "../user-menu/UserMenu";
 //import useMobile from "../hooks/useMobile";
 //import { DisplayPrice } from "../utils/DisplayPrice";
-import DisplayCartItem from "../display-card/DisplayCartItem";
+import DisplayCartItem from '../display-card/DisplayCartItem'
 //import {useSelector} from "react-redux";
 //import {useGlobalContext} from "../provider/GlobalProvider";
-import { useLocation, useNavigate } from "react-router-dom";
-import { FaRegCircleUser } from "react-icons/fa6";
-import { BsCart4 } from "react-icons/bs";
-import { GoTriangleDown, GoTriangleUp } from "react-icons/go";
+import { FaRegCircleUser } from 'react-icons/fa6'
+import { BsCart3 } from 'react-icons/bs'
+import { GoTriangleDown, GoTriangleUp } from 'react-icons/go'
 
 export default function Header() {
   //const [isMobile] = useMobile();
@@ -20,19 +20,74 @@ export default function Header() {
   //const isSearchPage = location.pathname === "/search";
   //const navigate = useNavigate();
   //const [openUserMenu, SetopenUserMenu] = useState(false);
-  //const [openCart, SetopenCart] = useState(false);
+  const [openCart, SetopenCart] = useState(false)
+  const router = useRouter()
 
+  const redirectToLoginPage = () => {
+    router.push('/login')
+  }
   return (
     <header className="h-24 lg:h-20 lg:shadow-md sticky top-0 z-40 flex flex-col justify-center gap-1 bg-white">
       {/* comment  */}
       <div className="container mx-auto flex items-center px-2 justify-between">
         {/*Logo */}
         <div className="h-full">
-          <Link href="/">
-            <img src="assets/images/logo.png" alt="logo" className="h-5" />
+          <Link href="/" className="h-full flex justify-center items-center">
+            <img
+              src="assets/images/logo.png"
+              width={170}
+              height={60}
+              alt="logo"
+              className="hidden lg:block"
+            />
+            <img
+              src="assets/images/logo.png"
+              width={120}
+              height={60}
+              alt="logo"
+              className="lg:hidden"
+            />
           </Link>
+        </div>
+        {/*Search */}
+        <div className="hidden lg:block">
+          <Search />
+        </div>
+
+        {/*login  and my cart  */}
+        <div className="">
+          {/** mobile version
+          <button
+            className="text-neutral-600 lg:hidden"
+            onClick={handleMobileUser}
+          >
+            <FaRegCircleUser size={26} />
+          </button> **/}
+
+          {/** desktop version */}
+          <div className="hidden lg:flex items-center gap-10">
+            {/** <UserMenu /> **/}
+
+            <button onClick={redirectToLoginPage} className="text-lg px-2">
+              Login
+            </button>
+
+            <button
+              onClick={() => SetopenCart(true)}
+              className="flex items-center gap-2 bg-red-700 hover:bg-red-900 px-3 py-2 rounded text-white"
+            >
+              {/** <add to cart  /> **/}
+              <div className="animate-bounce">
+                <BsCart3 size={26} />
+              </div>
+              <div className="font-bold text-sm">
+                {' '}
+                <p> My Cart</p>
+              </div>
+            </button>
+          </div>
         </div>
       </div>
     </header>
-  );
+  )
 }
--- client/components/search/Search.jsx
+++ client/components/search/Search.jsx
@@ -1,39 +1,20 @@
-import React from "react";
-import { IoSearch } from "react-icons/io5";
-import { Link, useNavigate, useLocation } from "react-router-dom";
-import { TypeAnimation } from "react-type-animation";
-import { FaArrowLeft } from "react-icons/fa";
-import useMobile from "../hooks/useMobile";
+import React from 'react'
+import Link from 'next/link'
+import { IoSearch } from 'react-icons/io5'
+import { FaArrowLeft } from 'react-icons/fa'
+import { TypeAnimation } from 'react-type-animation'
 
 export default function Search() {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const [isSearchPage, setIsSearchPage] = useState(false);
-  const [isMobile] = useMobile();
-  const params = useLocation();
-  const searchText = params.search.slice(3);
-
-  useEffect(() => {
-    const isSearch = location.pathname === "/search";
-    setIsSearchPage(isSearch);
-  }, [location]);
-
-  const redirectToSearchPage = () => {
-    navigate("/search");
-  };
-
-  const handleOnChange = (e) => {
-    const value = e.target.value;
-    const url = `/search?q=${value}`;
-    navigate(url);
-  };
+  const isMobile = false
+  const isSearchPage = false
+  const searchText = ''
 
   return (
-    <div className="w-full  min-w-[300px] lg:min-w-[420px] h-11 lg:h-12 rounded-lg border overflow-hidden flex items-center text-neutral-500 bg-slate-50 group focus-within:border-primary-200 ">
+    <div className="w-full min-w-[300px] lg:min-w-[420px] h-11 lg:h-12 rounded-lg border overflow-hidden flex items-center text-neutral-500 bg-slate-50 group focus-within:border-primary-200">
       <div>
         {isMobile && isSearchPage ? (
           <Link
-            to={"/"}
+            href="/"
             className="flex justify-center items-center h-full p-2 m-1 group-focus-within:text-primary-200 bg-white rounded-full shadow-md"
           >
             <FaArrowLeft size={20} />
@@ -46,10 +27,7 @@ export default function Search() {
       </div>
       <div className="w-full h-full">
         {!isSearchPage ? (
-          <div
-            onClick={redirectToSearchPage}
-            className="w-full h-full flex items-center"
-          >
+          <div className="w-full h-full flex items-center">
             <TypeAnimation
               sequence={[
                 'Search "milk"',
@@ -58,7 +36,7 @@ export default function Search() {
                 1000,
                 'Search "sugar"',
                 1000,
-                'Search "panner"',
+                'Search "couscous"',
                 1000,
                 'Search "chocolate"',
                 1000,
@@ -83,11 +61,10 @@ export default function Search() {
               autoFocus
               defaultValue={searchText}
               className="bg-transparent w-full h-full outline-none"
-              onChange={handleOnChange}
             />
           </div>
         )}
       </div>
     </div>
-  );
+  )
 }
