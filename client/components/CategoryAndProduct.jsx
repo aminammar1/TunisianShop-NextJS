abcbe6da17ce3e1d8ceca6f2db198b235909bae7
aminammar1 <ammar.mohamdamine@gmail.com>
2025-02-10 19:23:29 +0100

    complete See All Product Page

--- client/components/CategoryAndProduct.jsx
+++ client/components/CategoryAndProduct.jsx
@@ -18,76 +18,69 @@ export default function CategoryAndProduct({ id, name }) {
   const subCategoryData = useSelector((state) => state.product.allSubCategory)
   const loadingCardNumber = new Array(6).fill(null)
 
-  const fetchCategoryWithProduct = async () => {
-    try {
-      setLoading(true)
-      const response = await Axios({
-        ...GlobalApi.ProductByCategory,
-        params: { _id: id },
-      })
+        const fetchCategoryWithProduct = async () => {
+          try {
+            setLoading(true)
+            const response = await Axios({
+              ...GlobalApi.ProductByCategory, params: { _id: id }, })
 
-      const { data: responseData } = response
+            const { data: responseData } = response
 
-      if (responseData.success) {
-        setData(responseData.data)
-      }
-    } catch (error) {
-      //AxiosToastError(error)
-      console.log(error)
-    } finally {
-      setLoading(false)
-    }
-  }
+            if (responseData.success) {
+              setData(responseData.data)
+            }
+          } catch (error) {
+            //AxiosToastError(error)
+            console.log(error)
+          } finally {
+            setLoading(false)
+          }
+        }
 
-  useEffect(() => {
-    fetchCategoryWithProduct()
-  }, [])
+        useEffect(() => {
+          fetchCategoryWithProduct()
+        }, [])
 
-  const handleScrollLeft = () => {
-    if (containerRef.current) {
-      containerRef.current.scrollLeft -= 200
-    }
-  }
+        const handleScrollLeft = () => {
+          if (containerRef.current) {
+            containerRef.current.scrollLeft -= 200
+          }
+        }
 
-  const handleScrollRight = () => {
-    if (containerRef.current) {
-      containerRef.current.scrollLeft += 200
-    }
-  }
+        const handleScrollRight = () => {
+          if (containerRef.current) {
+            containerRef.current.scrollLeft += 200
+          }
+        }
 
-  const handleRedirectProductListpage = () => {
-    const subcategory = subCategoryData.find((sub) => {
-      const filterData = sub.category.some((c) => {
-        return c._id == id
-      })
+        const handleRedirectProductListpage = () => {
+          
+          const subcategory = subCategoryData.find((sub) => sub.category.includes(id))
+          
+          const url = `/productList/${valideURLConvert(name)}-${id}/${valideURLConvert(subcategory?.name)}-${subcategory?._id}`
+          return url
+        }
 
-      return filterData ? true : null
-    })
-    const url = `/${valideURLConvert(name)}-${id}/${valideURLConvert(
-      subcategory?.name
-    )}-${subcategory?._id}`
-
-    return url
-  }
-
-  const redirectURL = handleRedirectProductListpage()
+        const redirectURL = handleRedirectProductListpage()
+        
 
   return (
     <div>
       <div className="container mx-auto p-4 flex items-center justify-between gap-4">
-        <h3 className="font-semibold text-lg md:text-xl">{name}</h3>
-        <Link
-          href={redirectURL}
-          className="text-red-600 hover:text-red-400"
-        >
-          See All
-        </Link>
-      </div>
+            <h3 className="font-semibold text-lg md:text-xl">{name}</h3>
+                <Link
+                  href={redirectURL}
+                  className="text-red-600 hover:text-red-400"
+                >
+                  See All
+                </Link>
+     </div>
+      
       <div className="relative flex items-center ">
-        <div
-          className=" flex gap-4 md:gap-6 lg:gap-8 container mx-auto px-4 overflow-x-scroll scrollbar-none scroll-smooth"
-          ref={containerRef}
-        >
+            <div
+              className=" flex gap-4 md:gap-6 lg:gap-8 container mx-auto px-4 overflow-x-scroll scrollbar-none scroll-smooth"
+              ref={containerRef}
+            >
           {loading &&
             loadingCardNumber.map((_, index) => {
               return (
@@ -104,19 +97,20 @@ export default function CategoryAndProduct({ id, name }) {
             )
           })}
         </div>
-        <div className="w-full left-0 right-0 container mx-auto  px-2  absolute hidden lg:flex justify-between">
-          <button
-            onClick={handleScrollLeft}
-            className="z-10 relative bg-white hover:bg-gray-100 shadow-lg text-lg p-2 rounded-full"
-          >
-            <FaAngleLeft />
-          </button>
-          <button
-            onClick={handleScrollRight}
-            className="z-10 relative  bg-white hover:bg-gray-100 shadow-lg p-2 text-lg rounded-full"
-          >
-            <FaAngleRight />
-          </button>
+          <div className="w-full left-0 right-0 container mx-auto  px-2  absolute hidden lg:flex justify-between">
+              <button
+                onClick={handleScrollLeft}
+                className="z-10 relative bg-white hover:bg-gray-100 shadow-lg text-lg p-2 rounded-full"
+              >
+                <FaAngleLeft />
+              </button>
+               
+                <button
+                  onClick={handleScrollRight}
+                  className="z-10 relative  bg-white hover:bg-gray-100 shadow-lg p-2 text-lg rounded-full"
+                >
+                  <FaAngleRight />
+                </button>
         </div>
       </div>
     </div>
