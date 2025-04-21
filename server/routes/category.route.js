ade1556c6fddb5c5bbb59b08beff97dd776402cb
aminammar1 <ammar.mohamdamine@gmail.com>
2025-04-21 16:32:23 +0100

    format code

--- server/routes/category.route.js
+++ server/routes/category.route.js
@@ -3,8 +3,8 @@ import auth from '../middleware/auth.js'
 import {
   AddCategory,
   GetCategories,
-  updateCategory, 
-  deleteCategory
+  updateCategory,
+  deleteCategory,
 } from '../controllers/category.controller.js'
 import admin from '../middleware/admin.js'
 
@@ -12,7 +12,7 @@ const router = express.Router()
 
 router.post('/add-category', auth, admin, AddCategory)
 router.get('/get-categories', GetCategories)
-router.put('/update-category' , auth , admin , updateCategory)
-router.delete ('/delete-category' , auth , admin , deleteCategory)
+router.put('/update-category', auth, admin, updateCategory)
+router.delete('/delete-category', auth, admin, deleteCategory)
 
 export default router
